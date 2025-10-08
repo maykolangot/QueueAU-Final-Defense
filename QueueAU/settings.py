6a0cdd90b8771ec7893ad6ef4cdaea41d4e9674c
Michael Magdosa <michael.magdosa.dev@gmail.com>
2025-10-09 01:56:37 +0800

    Update settings.py

--- QueueAU/settings.py
+++ QueueAU/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-%mhsz76ph5v_8^xl8wj$zo=wz&#-cyv_*dirc%p_p9oe#%_!ui
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "queueau-alpha.onrender.com"]
 
 
 # Application definition
